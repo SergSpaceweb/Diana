dbf28f83a6ebf92303889b65f6026d7feb97d3af
SergSpaceweb <34378350+SergSpaceweb@users.noreply.github.com>
2018-05-21 13:02:54 +0300

    Add files via upload

---
+++ app/js/common.js
@@ -0,0 +1,109 @@
+$(function() {
+
+	$(".header-top__nav-btn").click( function() {
+		$(this).toggleClass("on");
+		$(".header-top__nav").slideToggle();
+	});
+
+	$(".header-top__currency").click( function() {
+		$(".currency-select").slideToggle();
+	});
+
+	$(".lang").click( function() {
+		$(".lang-select").slideToggle();
+	});
+
+
+
+
+
+	$('.menu-main').find('.menu-main__item a').click(function( event ){
+		event.preventDefault();
+		
+      //Expand or collapse this panel
+      $(this).next().slideToggle('fast');
+
+      //Hide the other panels
+      $(".menu-sub-1").not($(this).next()).slideUp('fast');
+
+  });
+
+
+	$('.gallery').slick({
+		dots: false
+	});
+
+	$('.slider').slick({
+		arrows: false,
+		dots: true,
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		responsive: [
+		{
+			breakpoint: 991,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+    ]
+});
+
+
+
+
+
+
+// 	var click = (function () {$(".accordion > li > a").click(function(){
+
+// 		if(false == $(this).next().is(':visible')) {
+// 			$('.accordion div').slideUp(100);
+// 		}
+// 		$(this).next().slideToggle(300);
+// 	});
+// });
+// 	var hover = (function () {$(".accordion > li > a").hover(function(){
+
+// 		if(false == $(this).next().is(':visible')) {
+// 			$('.accordion div').slideUp(100);
+// 		}
+// 		$(this).next().slideToggle(300);
+// 	});
+// });
+
+
+// 		if($(window).width() >= 768){
+// 			hover();
+// 		} else
+// 		click();
+	
+	// $(window).resize(function () {
+	// 	if($(window).width() >= 768){
+	// 		hover();
+	// 	} else
+	// 	click();
+	// });
+
+
+
+	
+
+
+
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var syntax        = 'sass'; // Syntax: sass or scss;
+
+var gulp          = require('gulp'),
+		gutil         = require('gulp-util' ),
+		sass          = require('gulp-sass'),
+		browsersync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require("gulp-notify");
+
+gulp.task('browser-sync', function() {
+	browsersync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// tunnel: true,
+		// tunnel: "diana", //Demonstration page: http://projectname.localtunnel.me
+	})
+});
+
+gulp.task('styles', function() {
+	return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
+	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browsersync.reload( {stream: true} ))
+});
+
+gulp.task('js', function() {
+	return gulp.src([
+		'app/libs/jquery/jquery.min.js',
+		'app/libs/js/slick.min.js',
+		'app/js/common.js', // Always at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	// .pipe(uglify()) // Minify js (opt.)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browsersync.reload({ stream: true }))
+});
+
+gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
+	gulp.watch('app/'+syntax+'/**/*.'+syntax+'', ['styles']);
+	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
+	gulp.watch('app/*.html', browsersync.reload)
+});
+
+gulp.task('default', ['watch']);
